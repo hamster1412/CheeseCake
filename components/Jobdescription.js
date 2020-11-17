b05558d5d230a6264c25590ac3dc39cec6ef86ab
andrecee.tnn <andrecee.tnn@gmail.com>
2020-11-18 02:28:54 +0700

    I removed a few things I thought I need but I actually don't

--- components/Jobdescription.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Jobdescription() {
-    return (
-        <div>
-            Description for the above.
-        </div>
-    )
-}
--- components/Jobtitle.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Jobtitle() {
-    return (
-        <div>
-            Title.
-        </div>
-    )
-}
