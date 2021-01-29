bc6eec406da7de790dcd99fbbeaba0c4451d79ed
An Tr <andrecee.tnn@gmail.com>
2021-01-29 18:21:28 +0700

    fix ui

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,11 @@
 import {Provider} from "next-auth/client";
-
+//this above really was an annoying error no one would ever expected
+//try remove {} and app crash
+import "tailwindcss/tailwind.css";
 export default function App({ Component, pageProps }) {
+  const { session } = pageProps;
   return (
-    <Provider session={pageProps.session}>
+    <Provider session={session}>
       <Component {...pageProps} />
     </Provider>
   );
