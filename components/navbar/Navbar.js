812bb9997f03bc29996d23e52691423e53f9bf24
tnngoan <ngoan.n.tr@gmail.com>
2021-12-20 17:07:27 +0700

    add search function with debounce service

--- components/navbar/Navbar.js
+++ components/navbar/Navbar.js
@@ -1,18 +1,19 @@
 import React from "react";
 import ProfilePopover from "./ProfilePopover";
 import Link from "next/link";
+import JobFilterForm from "./JobFilterForm";
 
 export default function Navbar() {
-  let key = "";
-  const search = (e) => {
-    key = e.target.value;
-    console.log("it's working", key);
-  };
+
+  function handleFilterChange(newFilter) {
+    console.log(newFilter);
+  }
+
   return (
     <div>
-      <nav className="bg-gray-800">
+      <nav className="bg-gray-800 py-1">
         <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
-          <div className="relative flex items-center justify-between h-16">
+          <div className="relative flex items-center justify-between ">
             <div className="flex-1 flex items-center justify-around sm:items-stretch sm:justify-start">
               <div className="hidden sm:block sm:ml-6">
                 <div className="flex justify-evenly space-4">
@@ -26,25 +27,25 @@ export default function Navbar() {
                     </Link>
                     <Link
                       href="/joblist"
-                      className="text-white px-3 py-2 rounded-md text-sm font-medium"
+                      className="text-white px-3 rounded-md text-sm font-medium"
                     >
                       <a className="text-white">Find Job</a>
                     </Link>
                     <Link
                       href="#"
-                      className="text-white hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                      className="text-white hover:bg-gray-700 hover:text-white px-3 rounded-md text-sm font-medium"
                     >
                       <a className="text-white">Company</a>
                     </Link>
                     <Link
                       href="#"
-                      className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                      className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 rounded-md text-sm font-medium"
                     >
                       <a className="text-white">Collaborate</a>
                     </Link>
                     <Link
                       href="#"
-                      className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                      className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 rounded-md text-sm font-medium"
                     >
                       <a className="text-white">Blog</a>
                     </Link>
@@ -53,12 +54,7 @@ export default function Navbar() {
               </div>
             </div>
             <div>
-              <input
-                onChange={(e) => search(e)}
-                placeholder="Search"
-                type="text"
-                className="px-3 rounded-md text-lg py-2 font-medium"
-              ></input>
+              <JobFilterForm onSubmit={handleFilterChange} />
             </div>
             <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
               <button className="bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
--- pages/joblist.js
+++ pages/joblist.js
@@ -1,12 +1,14 @@
 import React from "react";
 const results = require("../data.json");
-import Job from "../components/Job";
+import Job from "../components/joblist/Job";
 import PaginatedItems from "../components/Pagination";
 import Layout from "../components/layout";
+import SalarySlider from "../components/joblist/SalarySlider";
 
 function joblist() {
   return (
     <Layout home>
+      <SalarySlider />
       <div className="grid grid-cols-3">
         {results.map((result) => {
           return <Job key={result.id} result={result}></Job>;
